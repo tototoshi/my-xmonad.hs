74609f9d13578c03ba6109d3ead8eff0fd4391a5
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2010-11-30 22:31:10 +0900

    don't use dmenu

--- xmonad.hs
+++ xmonad.hs
@@ -76,9 +76,6 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- launch a terminal
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
-    -- launch dmenu
-    , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
-
     -- launch gmrun
     , ((modm,               xK_p     ), spawn "gmrun")
 
